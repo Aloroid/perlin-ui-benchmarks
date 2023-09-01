37df44e32fc6d59d7327532885e5afd7de25e59c
Aloroider <70312917+Aloroid@users.noreply.github.com>
2023-09-02 00:04:54 +0200

    sent

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -1,1 +1,15 @@
-print("Hello world, from client!")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local StarterGui = game:GetService("StarterGui")
+
+workspace.CurrentCamera.FieldOfView = 1
+workspace.CurrentCamera.CFrame = CFrame.fromMatrix(Vector3.zero, Vector3.zero, Vector3.zero)
+
+StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All, false)
+
+local ui_component = "vide"
+local component = ReplicatedStorage.Shared.Components:FindFirstChild(ui_component)
+
+assert(component, `component {ui_component} does not exist`)
+
+print('running')
+require(component)()
