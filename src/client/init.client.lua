10b45ebb58850eb1a3652c4a05bee0aaad5da5e8
Aloroider <70312917+Aloroid@users.noreply.github.com>
2023-09-09 12:34:12 +0200

    updated benchmarks + fps counter

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -1,15 +1,24 @@
+local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local StarterGui = game:GetService("StarterGui")
+
+local PARENT_TO_PLAYERGUI = false
 
 workspace.CurrentCamera.FieldOfView = 1
 workspace.CurrentCamera.CFrame = CFrame.fromMatrix(Vector3.zero, Vector3.zero, Vector3.zero)
 
 StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All, false)
 
-local ui_component = "pract"
+local ui_component = "react_state"
 local component = ReplicatedStorage.Shared.Components:FindFirstChild(ui_component)
+local parent = if PARENT_TO_PLAYERGUI then Players.LocalPlayer.PlayerGui else Instance.new("Folder")
+
+Players.LocalPlayer.PlayerGui:ClearAllChildren()
 
 assert(component, `component {ui_component} does not exist`)
 
-print('running')
-require(component)()
+print('running', ui_component)
+
+require(component)(parent)
+task.wait(1)
+require(ReplicatedStorage.Shared.Components.display_fps)()
